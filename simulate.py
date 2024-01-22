920593f8f25e110e43d76ce96998c70e42cd162f
jordanvtskier1 <jmgottli@uvm.edu>
2024-01-22 18:47:09 -0500

    playing with time and physics.

--- simulate.py
+++ simulate.py
@@ -1,7 +1,13 @@
 import pybullet as p
+import time
 
 physicsClient = p.connect(p.GUI)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
+for _ in range(1000):
+    p.stepSimulation()
+    time.sleep(1/60)
+    print(_)
 p.disconnect()
 
 
