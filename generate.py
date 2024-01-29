04a26e33ce41dc91a01a259c89cbbd17d0cb5517
jordanvtskier1 <jmgottli@uvm.edu>
2024-01-29 13:43:21 -0500

    finished towers

--- generate.py
+++ generate.py
@@ -1,8 +1,34 @@
 import pyrosim.pyrosim as pyrosim
 
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+length = 1
+width = 1
+height = 1
+
+x=-3
+y=-3
+z=.5
+
+i = 1
+s = 1
+
+
+while x<2:
+    x+=1
+    y=-3
+    while y<2:
+        while z <10:
+            pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length/s,width/s,height/s])
+            z+=1
+            s+=.1
+            i+=1
+        y+=1
+        s=1
+        z=.5
+    
+#pyrosim.Send_Cube(name="Box2", pos=[1,y,1.5] , size=[length,width,height])
+
 
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -1,11 +1,14 @@
 import pybullet as p
 import time
+import pybullet_data
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 #p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
 p.setGravity(0,0,-9.8)
-p.loadSDF("box.sdf")
+planeId = p.loadURDF("plane.urdf")
+p.loadSDF("boxes.sdf")
 for _ in range(1000):
     p.stepSimulation()
     time.sleep(1/60)
