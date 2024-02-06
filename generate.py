d6475225d3353e11215dddbbccf4dd90fef47ca1
jordanvtskier1 <jmgottli@uvm.edu>
2024-02-05 20:23:42 -0500

    done with joints

--- generate.py
+++ generate.py
@@ -15,6 +15,21 @@ def Create_World():
     
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
+    
+    pyrosim.Send_Cube(name="Torso", pos=[1.5,0,1.5] , size=[length,width,height])
+
+    pyrosim.Send_Joint(name="Torso_Backleg", parent="Torso", child="Backleg", type="revolute", position=[1,0,1])
+    
+    pyrosim.Send_Cube(name="Backleg", pos=[-0.5,0,-0.5] , size=[length,width,height])
+
+    pyrosim.Send_Joint(name="Torso_Frontleg", parent="Torso", child="Frontleg", type="revolute", position=[2,0,1])
+    
+    pyrosim.Send_Cube(name="Frontleg", pos=[0.5,0,-0.5] , size=[length,width,height])
+
+
+
+    """
+    pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(name="Link0", pos=[0,0,.5] , size=[length,width,height])
 
     pyrosim.Send_Joint(name="Link0_Link1", parent="Link0", child="Link1", type="revolute", position=[0,0,1])    
@@ -24,9 +39,24 @@ def Create_Robot():
     pyrosim.Send_Joint(name="Link1_Link2", parent="Link1", child="Link2", type="revolute", position=[0,0,1])    
 
     pyrosim.Send_Cube(name="Link2", pos=[0, 0, 0.5] , size=[length,width,height])
+    
+    pyrosim.Send_Joint(name="Link2_Link3", parent="Link2", child="Link3", type="revolute", position=[0,0.5,0.5])    
 
+    pyrosim.Send_Cube(name="Link3", pos=[0, 0.5, 0] , size=[length,width,height])
 
- 
+    pyrosim.Send_Joint(name="Link3_Link4", parent="Link3", child="Link4", type="revolute", position=[0,1,0])    
+
+    pyrosim.Send_Cube(name="Link4", pos=[0, 0.5, 0] , size=[length,width,height])
+
+    pyrosim.Send_Joint(name="Link4_Link5", parent="Link4", child="Link5", type="revolute", position=[0,.5,-0.5])    
+
+    pyrosim.Send_Cube(name="Link5", pos=[0, 0, -0.5] , size=[length,width,height])
+
+    pyrosim.Send_Joint(name="Link5_Link6", parent="Link5", child="Link6", type="revolute", position=[0,0,-1])    
+
+    pyrosim.Send_Cube(name="Link6", pos=[0, 0, -0.5] , size=[length,width,height])
+    """
+
 
     pyrosim.End()
 
